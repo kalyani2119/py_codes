0eb2b90f5e07de351f39b7381b6c13090a97ec66
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2021-11-02 21:16:50 +0530

    Update rft.py

--- rft.py
+++ rft.py
@@ -1,1 +1,21 @@
+#Reverse Floyd's Triangle
+" " "
+example if n=5 and a=10
 
+15
+14 13
+12 11 10
+9  8  7  6
+5  4  3  2  1
+
+" " "
+n=int(input("Enter the number of rows"))
+a=0
+for i in range(n):
+  a=a+1
+  b=n+a
+  for i in range(n):
+    for i in range(i+1):
+      print(format(b,"<3"),end="")
+      b=b-1
+  print()
